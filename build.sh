e3370a68e1d98c4c408b816f423c19716f0a52da
John Melvin <johnmelvin2014@gmail.com>
2023-04-11 13:14:01 -0400

    Build script: delete all except .gitignore

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rm -rf build
-mkdir build
+shopt -s extglob
+
 cd build
+rm -- !(.gitignore)
 
 cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 cmake --build .
