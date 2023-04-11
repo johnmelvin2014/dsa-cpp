3e876bb1140db06918f27dd72da1fdd77b7ad909
John Melvin <johnmelvin2014@gmail.com>
2023-04-11 13:07:44 -0400

    Build: include compile compands

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rm -rf build
 mkdir build
 cd build
 
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ..
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 cmake --build .
