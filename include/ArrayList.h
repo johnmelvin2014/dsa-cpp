025d7d3230cdabb1740f673d21652be55389b8e3
John Melvin <johnmelvin2014@gmail.com>
2023-04-11 17:45:03 -0400

    Add at index

--- include/ArrayList.h
+++ include/ArrayList.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <memory>
+#include <stdexcept>
+#include <string>
 
 template <typename T>
 class ArrayList {
@@ -8,58 +10,92 @@ class ArrayList {
     static constexpr int INITIAL_CAPACITY{10};
 
   private:
-    unsigned int size{};
-    unsigned int current_capacity{INITIAL_CAPACITY};
-    std::unique_ptr<T> backing_array{};
+    int size{};
+    int current_capacity{INITIAL_CAPACITY};
+    std::unique_ptr<T[]> backing_array{};
 
   public:
     ArrayList();
     ArrayList(const ArrayList &other);
 
-    void add_at_index(unsigned int index, T data);
+    void add_at_index(int index, T data);
     void add_at_front(T data);
     void add_to_back(T data);
 
-    T &remove_at_index(unsigned int index);
+    T &remove_at_index(int index);
     T &remove_from_front();
     T &remove_from_back();
 
-    T &get(unsigned int index);
+    T &get(int index);
     bool is_empty();
     void clear();
 
-    unsigned int get_size();
-    unsigned int get_current_capacity();
-    std::unique_ptr<T> get_backing_array();
+    int get_size();
+    int get_current_capacity();
 
   private:
     void copy_to_expanded_backing_array();
+    void validateIndexInRange(int index);
 };
 
 template <typename T>
 ArrayList<T>::ArrayList()
     : size{},
       current_capacity{ArrayList::INITIAL_CAPACITY},
-      backing_array{std::make_unique<T>(ArrayList::INITIAL_CAPACITY)} {}
+      backing_array{std::make_unique<T[]>(ArrayList::INITIAL_CAPACITY)} {}
 
 template <typename T>
-unsigned int ArrayList<T>::get_size() {
+void ArrayList<T>::add_at_index(int index, T data) {
+    validateIndexInRange(index);
+
+    bool arrayIsFull{size == current_capacity};
+    if (arrayIsFull) {
+        copy_to_expanded_backing_array();
+    }
+
+    for (int j{size - 1}; j >= index; --j) {
+        backing_array[j + 1] = std::move(backing_array[j]);
+    }
+    backing_array[index] = data;
+    size++;
+}
+
+template <typename T>
+T &ArrayList<T>::get(int index) {
+    validateIndexInRange(index);
+
+    return backing_array[index];
+}
+
+template <typename T>
+int ArrayList<T>::get_size() {
     return size;
 }
 
 template <typename T>
-unsigned int ArrayList<T>::get_current_capacity() {
+int ArrayList<T>::get_current_capacity() {
     return current_capacity;
 }
 
 template <typename T>
 void ArrayList<T>::copy_to_expanded_backing_array() {
     const int old_capacity{current_capacity};
-    std::unique_ptr<T> old_backing_array = std::make_unique(std::move(backing_array));
+    std::unique_ptr<T[]> old_backing_array{std::move(backing_array)};
 
     current_capacity *= 2;
-    backing_array = std::make_unique<T>(current_capacity);
+    backing_array = std::make_unique<T[]>(current_capacity);
     for (int i{}; i < old_capacity; ++i) {
         backing_array[i] = std::move(old_backing_array[i]);
+    }
+}
+
+template <typename T>
+void ArrayList<T>::validateIndexInRange(int index) {
+    if (index < 0) {
+        std::string error_msg{"Cannot add at index less than 0"};
+        throw std::out_of_range(error_msg);
+    } else if (index > size) {
+        std::string error_msg{"Cannot add at index > size"};
+        throw std::out_of_range(error_msg);
     }
 }
--- tests/test_ArrayList.cpp
+++ tests/test_ArrayList.cpp
@@ -7,6 +7,22 @@ TEST(ArrayListConstruction, ParmeterSetting) {
     EXPECT_EQ(array_list.get_current_capacity(), array_list.INITIAL_CAPACITY);
 }
 
+TEST(ArrayListAddAtIndex, BasicAddAtIndex) {
+    ArrayList<int> array_list{};
+    array_list.add_at_index(0, 2);
+    array_list.add_at_index(0, 1);
+    array_list.add_at_index(2, 4);
+    array_list.add_at_index(2, 3);
+    array_list.add_at_index(0, 0);
+
+    EXPECT_EQ(array_list.get_size(), 5);
+    int expected[]{0, 1, 2, 3, 4};
+    for (int i{}; i < 5; ++i) {
+        int actual{array_list.get(i)};
+        EXPECT_EQ(actual, expected[i]) << "Arrays differed at " << i;
+    }
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     ArrayList<int> array_list;
