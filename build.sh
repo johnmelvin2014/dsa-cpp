f49fc1dcb87bccb17db9c2b670f2f92f1cc2ebad
John Melvin <johnmelvin2014@gmail.com>
2023-04-11 13:50:51 -0400

    Build script bug patch

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 shopt -s extglob
 
 cd build
-rm -- !(.gitignore)
+rm -rf -- !(.gitignore)
 
 cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 cmake --build .
